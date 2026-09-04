aa45a8d548232424394767ec95a99f3ecb31880f
orr25 <121784012+orr25@users.noreply.github.com>
2026-09-04 16:48:57 -0700

    Create events.js

---
+++ data/events.js
@@ -0,0 +1,23 @@
+window.DEVRIES_EVENTS = [
+
+  {
+    building: "Devries",
+    date: "2026-10-5",
+    time: "8:45",
+    durationMinutes: 00,
+    title: "Orientation Day",
+    location: "Devries",
+    description: "Orientation Day! Settle into your new dorms!"
+  },
+
+  {
+    building: "4X",
+    date: "2026-09-12",
+    time: "19:30",
+    durationMinutes: 120,
+    title: "Movie Night",
+    location: "4th Floor Lounge",
+    description: "Movie and snacks."
+  }
+
+];
