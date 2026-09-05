7ad4454ed353e541aa70d572c77c775b04f3c074
orr25 <121784012+orr25@users.noreply.github.com>
2026-09-05 00:46:50 -0700

    Update events.js

--- data/events.js
+++ data/events.js
@@ -9,5 +9,13 @@ window.DEVRIES_EVENTS = [
     location: "Online",
     description: " Please complete this form so I can get to know you a lot better! https://forms.cloud.microsoft/r/w05g9zJyZS?origin=lprLink"
   }
-
+,   {
+    building: "ALL",
+    date: "2026-10-5",
+    time: "09:00",
+    durationMinutes: 15,
+    title: "Orientation Day!",
+    location: "Devries",
+    description: "Settle into your new home! Take your time and get used to your dorm, and make everything cozy!"
+  }
 ];
