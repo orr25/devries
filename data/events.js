c0a0932cfcc6d48d4dba5753eeeac222cbb0d1f9
orr25 <121784012+orr25@users.noreply.github.com>
2026-09-05 00:41:09 -0700

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,13 +1,14 @@
 window.DEVRIES_EVENTS = [
 
   {
-    building: "ALL",
+    building: "4X",
     date: "2026-09-12",
     time: "20:00",
     durationMinutes: 15,
     title: "Get to know questionnaire",
     location: "Online",
-    description: "https://forms.cloud.microsoft/r/w05g9zJyZS?origin=lprLink"
+    description: " Please complete this form so I can get to know you a lot better! 
+      https://forms.cloud.microsoft/r/w05g9zJyZS?origin=lprLink"
   }
 
 ];
