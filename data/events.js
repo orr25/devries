53f3e2e185760cedfcfca1320ba22f28bff90b78
orr25 <121784012+orr25@users.noreply.github.com>
2026-09-04 20:17:51 -0700

    Update events.js

--- data/events.js
+++ data/events.js
@@ -4,10 +4,10 @@ window.DEVRIES_EVENTS = [
     building: "ALL",
     date: "2026-09-12",
     time: "20:00",
-    durationMinutes: 120,
-    title: "TEST — Devries Night",
-    location: "Devries",
-    description: "Testing the event system."
+    durationMinutes: 15,
+    title: "Get to know questionnaire",
+    location: "Online",
+    description: "https://forms.cloud.microsoft/r/w05g9zJyZS?origin=lprLink"
   }
 
 ];
