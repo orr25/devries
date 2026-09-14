e486207f4b11e953d31a42f6337d8b4446a3c60e
orr25 <121784012+orr25@users.noreply.github.com>
2026-09-14 16:17:30 -0700

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,21 +1,12 @@
 window.DEVRIES_EVENTS = [
 
   {
-    building: "4X",
-    date: "2026-09-12",
-    time: "20:00",
-    durationMinutes: 15,
-    title: "Get to know questionnaire!",
-    location: "Online",
-    description: " Please complete this form so I can get to know you a lot better! https://forms.cloud.microsoft/r/w05g9zJyZS?origin=lprLink"
-  }
-,{
-  building: "ALL",
-    date: "2026-09-06",
-    time: "17:00",
+    building: "ALL",
+    date: "2026-09-16",
+    time: "21:00",
     durationMinutes: 60,
-    title: "Dorm Dinner",
-    location: "Caf",
-    description: "Meet in Lounge 5:10, and walk over together to dinner"
-}
+    title: "DORM NIGHT: Watermelon Rugby",
+    location: "Devries",
+    description: " All of the Devries guys will be competing in Watermelon Rugby for Dorm Night!"
+  }
 ];
